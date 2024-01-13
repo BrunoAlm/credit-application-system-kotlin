3e3395542a56e99f8ac65f16458b6bd803f17d15
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-13 14:56:04 -0300

    feat: persistent layer

---
+++ src/main/kotlin/online/brunoalm/credit/application/system/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package online.brunoalm.credit.application.system.repository
+
+import online.brunoalm.credit.application.system.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long> {
+}
