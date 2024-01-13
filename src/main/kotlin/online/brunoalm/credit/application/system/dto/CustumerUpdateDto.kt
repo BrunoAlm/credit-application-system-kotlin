450e0dfe725a901ff2ab2d7eeb6d91387c5ca445
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-13 17:09:33 -0300

    feat: add view layer

--- src/main/kotlin/online/brunoalm/credit/application/system/dto/CustumerUpdateDto.kt
+++ src/main/kotlin/online/brunoalm/credit/application/system/dto/CustumerUpdateDto.kt
@@ -1,2 +1,21 @@
-package online.brunoalm.credit.application.system.dto 
+package online.brunoalm.credit.application.system.dto
 
+import online.brunoalm.credit.application.system.entity.Customer
+import java.math.BigDecimal
+
+data class CustumerUpdateDto(
+    val firstName: String,
+    val lastName: String,
+    val income: BigDecimal,
+    val zipCode: String,
+    val street: String,
+) {
+    fun toEntity(customer: Customer): Customer {
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.zipCode = this.zipCode
+        customer.address.street = this.street
+        return customer
+    }
+}
