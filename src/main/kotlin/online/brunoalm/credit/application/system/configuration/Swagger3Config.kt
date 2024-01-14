56eb065149dc525fe450d73197676e871e53e432
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2024-01-14 15:41:47 -0300

    feat: add tests fix: things broke

--- src/main/kotlin/online/brunoalm/credit/application/system/configuration/Swagger3Config.kt
+++ src/main/kotlin/online/brunoalm/credit/application/system/configuration/Swagger3Config.kt
@@ -1,6 +1,7 @@
 package online.brunoalm.credit.application.system.configuration
 
 
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,6 +12,11 @@ class Swagger3Config {
     fun publicApi(): GroupedOpenApi? {
         return GroupedOpenApi.builder()
             .group("springcreditapplicationsystem-public")
+            .addOpenApiCustomizer {
+                it.info.title = "Credit Application System"
+                it.info.description = "Projeto da DIO de Spring API com Kotlin"
+                it.info.version = "0.1.0"
+            }
             .pathsToMatch("/api/customers/**", "/api/credits/**")
             .build()
     }
